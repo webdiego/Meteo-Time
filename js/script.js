802e6bdf5537957f74279c3508dd30f2258596e8
Your Name <webdiegomassarini@gmail.com>
2021-02-02 00:36:26 +1100

    created a loading phase during the geo-localization

--- js/script.js
+++ js/script.js
@@ -1,43 +1,78 @@
-// const apiKey = "n9YazxwN70lUO9Te7hZZYBOTYe9EXxJs";
+const apiKey = "n9YazxwN70lUO9Te7hZZYBOTYe9EXxJs";
 const locationCity = document.querySelector(".location-city-text");
 const locationTemperature = document.querySelector(
   ".location-temperature-text"
 );
 const geolocalBtn = document.querySelector(".geolocal-icon");
-const cover = document.querySelector('.cover')
-console.log(geolocalBtn);
-// getWeather('milan')
+const cover = document.querySelector(".cover");
+const loading = document.querySelector(".loading");
 
+//FUNCTION CAPITALIZE WORD
 const firstUpper = function (word) {
   return word[0].toUpperCase() + word.slice(1);
 };
 
-//GET POSITION GEOLOCALIZATION
+//GET CORDS
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-//GET CITY FROM LOCATIZATION
+//GET CITY FROM CORDS AND CHANGING UI
 const getCity = async function (lat, lng) {
-  // cover.classList.add('none')
   try {
     const geo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
     if (!geo.ok) throw new Error("Getting problem get location");
     const dataGeo = await geo.json();
     const city = dataGeo.city.toLowerCase();
+
     locationCity.innerHTML = firstUpper(city);
     locationTemperature.textContent = 23;
 
-    //Removing cover from
-    cover.classList.remove('none')
+    loading.classList.add("none");
+    cover.classList.remove("none");
+
+    console.log(city);
   } catch (err) {
     console.log(err);
   }
 };
 
+
+const getCityApi = async function(city){
+  try{
+    const data = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`)
+    const res = await data.json()
+
+    console.log(res);
+  }catch(err){
+    console.log(err);
+  }
+
+}
+
+getCityApi('milan')
+
+// const getWeatherToday =async function () {
+//   try{
+//     const data = await fetch(`https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiKey}`)
+
+//   }catch(err){
+//     console.log(err);
+//   }
+// };
+// const getWeatherNext5Days = [];
+
+
+
+
+
+
 const geolocal = function () {
+  loading.classList.remove("none");
+  cover.classList.add("none");
+
   getPosition().then((res) => {
     const { latitude: lat, longitude: lng } = res.coords;
 
