4508fbb262f0c823ac1ed0ffc9636f285bbbc185
Your Name <webdiegomassarini@gmail.com>
2021-02-01 11:13:26 +1100

    html structure and style completed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 
-const apiKey = "n9YazxwN70lUO9Te7hZZYBOTYe9EXxJs";
+// const apiKey = "n9YazxwN70lUO9Te7hZZYBOTYe9EXxJs";
 
-const getCity = async function(city){
-  try {
-    const res = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`);
+// const getCity = async function(city){
+//   try {
+//     const res = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`);
 
-    const data = await res.json();
+//     const data = await res.json();
     
-    console.log(data[0]);
-    return data[0]
+//     console.log(data[0]);
+//     getWeatherToday(data[0].Key)
+//   }catch(err){
+//     console.log(err)
+//   }
 
-  }catch(err){
-    console.log(err)
-  }
+// }
 
-}
+// getCity('rome')
 
-getCity('rome')
-// const getWeather = async function(Key){
+// const getWeatherToday = async function(Key){
+//   try{
+//    const res = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${apiKey}`);
+//    const data = await res.json();
+//    console.log(data);
+//   }catch(err){
+//     console.log(err);
+//   }
+//  }
+ 
+
+// const getWeather5Days = async function(Key){
 //  try{
-//   const res = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${Key}?apikey=${apiKey}`);
+//   const res = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${Key}?apikey=${apiKey}`);
 //   const data = await res.json();
 //   console.log(data);
 //  }catch(err){
@@ -27,6 +38,6 @@ getCity('rome')
 //  }
 // }
 
-
-// getWeather('214046')
+// getWeatherToday('214046')
+// getWeather5Days('214046')
 
