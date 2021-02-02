442613e54927af21c3428a4ba30d864b0d032c98
Your Name <webdiegomassarini@gmail.com>
2021-02-02 14:11:39 +1100

    removed accuWeather Api and added weatherApi / added  main funtionalities

--- js/script.js
+++ js/script.js
@@ -1,29 +1,91 @@
 //API KEY
-const apiKey = "n9YazxwN70lUO9Te7hZZYBOTYe9EXxJs";
+const apiKey = "5Tn027egUBEXjIoaepwAxAUSXxTA5Nnz";
+const apiKey2 = '5f1002c6c18243bc98814327210202'
 
 //LOCATION UI
 const locationCity = document.querySelector(".location-city-text");
 const locationTemperature = document.querySelector(
   ".location-temperature-text"
 );
+const sunRiseEl = document.querySelector('.details-sunrise')
+const sunSetEl = document.querySelector('.details-sunset')
+const humidityEl = document.querySelector('.details-humidity')
+const windEl = document.querySelector('.details-wind')
+
+const app = document.querySelector('.app')
+//SEARCH LOCATION BTN
 const geolocalBtn = document.querySelector(".geolocal-icon");
 const searchBtn = document.querySelector('.search-icon-2')
+const searchInput = document.querySelector('.search-input')
 
 //UI ELEMENTS
 const cover = document.querySelector(".cover");
 const loading = document.querySelector(".loading");
 
-//FUNCTION CAPITALIZE WORD
+//*FUNCTION CAPITALIZE WORD
 const firstUpper = function (word) {
   return word[0].toUpperCase() + word.slice(1);
 };
-
-//GET CORDS
+//*FUNCTION DAY-NIGHT
+const isDayTime = function(dayTime){
+  if(dayTime === 1){
+  app.classList.remove('bg-night')
+  app.classList.add('bg-day')
+  
+  }else{
+    app.classList.remove('bg-day')
+    app.classList.add('bg-night')
+  }
+}
+const addLoad =function(){
+  loading.classList.remove("none");
+  cover.classList.add("none");
+}
+const removeLoad = function() {
+  loading.classList.add("none");
+  cover.classList.remove("none");
+}
+//*GET CORDS
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
+
+
+const getCityApi = async function(city){
+  try{
+     addLoad()
+    //Current weather
+    const dataToday = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey2}&q=${city}`)
+    const resToday = await dataToday.json()
+    console.log(resToday);
+    const {current: {temp_c : temperature}} =resToday
+    locationTemperature.innerHTML= `${temperature}`
+    //ADD Location name in UI
+    const {location : {name: name}} =resToday
+    locationCity.innerHTML= `${name}`
+    //ADD HUMIDITY ,WIND,DAYTIME
+    const {current :{humidity: humidity , wind_kph : wind , is_day: day}} = resToday
+    isDayTime(day)
+    humidityEl.innerHTML = `${humidity} %`
+    windEl.innerHTML = `${wind} Km/h`
+
+    //Astronomy - ADD Sunrise/Sunset
+    const data = await fetch(`http://api.weatherapi.com/v1/astronomy.json?key=${apiKey2}&q=${city}`)
+    const res = await data.json()
+    const {astronomy :{astro :{sunrise : sun, sunset: set}}} = res
+    sunRiseEl.innerHTML = `${sun.toLowerCase()}`
+    sunSetEl.innerHTML = `${set.toLowerCase()}`
+
+    //Remove loading once we get all the data
+    removeLoad()
+
+  }catch(err){
+    console.error(`Something wrong : ${err.message}🌚`);
+  }
+
+}
 
 //GET CITY FROM CORDS AND CHANGING UI
 const getCity = async function (lat, lng) {
@@ -31,65 +93,39 @@ const getCity = async function (lat, lng) {
     const geo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
     if (!geo.ok) throw new Error("Getting problem get location");
     const dataGeo = await geo.json();
-    const city = dataGeo.city.toLowerCase();
 
-    locationCity.innerHTML = firstUpper(city);
-    locationTemperature.textContent = 23;
+    const { poi : {addr_city: cityGeo , addr_suburb : subCity}}= dataGeo
+    // const city = dataGeo.city.toLowerCase();
 
-    loading.classList.add("none");
-    cover.classList.remove("none");
+    getCityApi(cityGeo)
+    locationCity.innerHTML = subCity
+    
+   removeLoad()
 
-    console.log(city);
   } catch (err) {
     console.log(err);
   }
 };
 
 
-const getCityApi = async function(city){
-  try{
-    const data = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`)
-    const res = await data.json()
-
-    console.log(res);
-  }catch(err){
-    console.log(err);
-  }
-
-}
-
-// getCityApi('milan')
-
-// const getWeatherToday =async function () {
-//   try{
-//     const data = await fetch(`https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${apiKey}`)
-
-//   }catch(err){
-//     console.log(err);
-//   }
-// };
-// const getWeatherNext5Days = [];
-
-
-
-
-
 
 const geolocal = function () {
-  loading.classList.remove("none");
-  cover.classList.add("none");
+  addLoad()
 
   getPosition().then((res) => {
     const { latitude: lat, longitude: lng } = res.coords;
 
-    getCity(lat, lng);
+  getCity(lat, lng);
+  
   });
 };
 
 geolocalBtn.addEventListener("click", geolocal);
 
 searchBtn.addEventListener('click' , function(e){
-  console.log(e);
-  loading.classList.add("none");
-    cover.classList.remove("none");
+  e.preventDefault()
+  
+  console.log(searchInput.value);
+  getCityApi(searchInput.value)
+ 
 })
